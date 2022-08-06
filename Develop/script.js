c1fffa4d8abb3b3a2630f5d5d60c65ade5aa16e7
David Healy <drtjprdave@gmail.com>
2022-08-06 12:41:40 -0600

    Recovered js.

--- Develop/script.js
+++ Develop/script.js
@@ -1,51 +1,77 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var number = "0123456789";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "abcdefghijklmnopqrstuvwxyz".toUpperCase();
+var specialChar = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-
-  var numberChar = "0123456789";
-  var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var lowerChars = "abcdefghijklmnopqrstuvwxyz";
-  var specialChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-  var allChar = numberChar, upperChars, lowerChars, specialChar;
-
-  
-function  getLower(){
- 
-}
-
-function  getUpper(){
-
-}
-
-function  getNumber(){
-
-}
-
-function  getSpecial(){
-
-}
-
-
-
   passwordText.value = password;
-
+  return;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
-  return 'Password'
+  
   // prompt user for length
+  let lengthPrompt = prompt("How long do you want you password to be? between 8 and 128 characters.")
+  console.log(lengthPrompt);  
 
-  // prompt user to confirm Character choices
+  if (lengthPrompt  < 8 || lengthPrompt > 128 ) {
+    alert("Your password needs to be in range of 8 and 128.");
+    let lengthPrompt = prompt("How long do you want you password to be? between 8 and 128 characters.");
+  }
+  
+  var container = ""
+  
+  let numCon = confirm("Do you want numbers?")
+  console.log(numCon);
+
+  if (numCon) {
+    container += number;
+    console.log(container);
+    }
+
+  let lowerCaseCon = confirm("Do you want lower case letters?")
+  console.log(lowerCase);  
+  if (lowerCaseCon) {
+    container += lowerCase;
+    console.log(container);
+
+    }
+
+  let upperCaseCon = confirm("Do you want upper case letters?")
+  console.log(upperCase);  
+  if (upperCaseCon) {
+    container += upperCase;
+    console.log(container);
+
+    }
+
+  let specialCharCon = confirm("Do you want special character?")
+  console.log(specialChar);
+  if (specialCharCon) {
+    container += specialChar;
+    console.log(container);
+
+    }
+
+  
+
+
+
+
 
   // generate the password using input
 
-  // return the generated password
+  // return 'Password'
+
+
+
 }
